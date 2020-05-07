1d37c054df77d5d920ddb870fa7b07e97307a5b2
Christopher Quinn <caquinn.86@gmail.com>
2020-05-07 07:21:05 -0700

    set up model to properly retrieve all associated attributes upon request, and added functionality to add and remove avs rows

--- models/attrVoiceSampleModel.js
+++ models/attrVoiceSampleModel.js
@@ -1,23 +1,25 @@
 const db = require('../data/dbConfig.js');
+const attribute = require('./attributesModel.js');
 
-const findAll = (id) => {
-  return db('attributes_voice_samples')
-    .where({sample_id: id})
-    .select("id");
+const findAll = async (id) => {
+  const associations = await db('attributes_voice_samples')
+    .where({voice_sample_id: id})
+  return Promise.all(associations.map(async assoc => {
+    return attribute.findById(assoc.attribute_id);
+  }))
 }
 
 const findById = (id) => {
   return db('attributes_voice_samples')
     .where({id})
-    .rightOuterJoin("attributes", "attribute_id", "attributes.id")
+    .rightOuterJoin("attributes", "attributes.id", "attribute_id")
     .select("type", "title", "description");
 }
 
 const add = async (data) => {
-  const [id] = await db('attributes_voice_samples')
+  return db('attributes_voice_samples')
     .insert(data)
     .returning('id');
-  return [id] ? true : false;
 }
 
 const remove = async (id) => {
