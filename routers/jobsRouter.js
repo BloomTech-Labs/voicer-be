a0de2f0828df6542a1fbe42c371ae9e58ff0794f
Christopher Quinn <caquinn.86@gmail.com>
2020-05-08 21:37:31 -0700

    fixed typo

--- routers/jobsRouter.js
+++ routers/jobsRouter.js
@@ -40,7 +40,7 @@ router.get('/:id', (req, res) => {
 router.post('/', authenticate, (req, res) => {
   const creatorId = req.dJwt.id;
   const job = req.body;
-  Jobs.createJobs(creatorId, job)
+  Jobs.createJob(creatorId, job)
     .then(created => {
       res.status(201).json(created)
     })
