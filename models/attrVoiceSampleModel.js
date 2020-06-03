b1a7cbad4feb70decaa5d505a1ae30e652ebd964
Christopher Quinn <caquinn.86@gmail.com>
2020-06-03 15:45:58 -0700

    Removed findAll and findById functions as they are not antiquated

--- models/attrVoiceSampleModel.js
+++ models/attrVoiceSampleModel.js
@@ -1,21 +1,6 @@
 const db = require('../data/dbConfig.js');
 const attribute = require('./attributesModel.js');
 
-const findAll = async (id) => {
-  const associations = await db('attributes_voice_samples')
-    .where({voice_sample_id: id})
-  return Promise.all(associations.map(async assoc => {
-    return attribute.findById(assoc.attribute_id);
-  }))
-}
-
-const findById = (id) => {
-  return db('attributes_voice_samples')
-    .where({id})
-    .rightOuterJoin("attributes", "attributes.id", "attribute_id")
-    .select("type", "title", "description");
-}
-
 const add = async (data) => {
   return db('attributes_voice_samples')
     .insert(data)
@@ -29,8 +14,6 @@ const remove = async (id) => {
 }
 
 module.exports = {
-  findAll,
-  findById,
   add,
   remove
 }
