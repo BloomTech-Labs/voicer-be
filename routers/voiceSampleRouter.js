ac26af9a2173c186275b75c1f72d7d702f10be5d
Christopher Quinn <caquinn.86@gmail.com>
2020-06-07 09:56:07 -0700

    offloaded s3 uploader to its own file for modular use

--- routers/voiceSampleRouter.js
+++ routers/voiceSampleRouter.js
@@ -1,27 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../middleware/authenticate.js');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const AWS = require('aws-sdk');
+const uploadS3 = require('../common/uploadS3.js');
 
 const voiceSample = require('../models/voiceSamplesModel.js');
-
-const s3 = new AWS.S3();
-
-const uploadS3 = multer({
-  storage: multerS3({
-    s3: s3,
-    acl: 'public-read',
-    bucket: process.env.S3_BUCKET,
-    metadata: (req, file, cb) => {
-      cb(null, {fieldName: file.fieldname})
-    },
-    key: (req, file, cb) => {
-      cb(null, Date.now().toString() + '-' + file.originalname)
-    }
-  })
-})
 
 // Get a list of voice samples for the specified user
 router.get('/:id', (req, res) => {
