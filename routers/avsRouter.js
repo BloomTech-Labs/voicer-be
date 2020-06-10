c818dbd8a5ec631e1b81bcfc4faf5eac8c334c7c
Christopher Quinn <caquinn.86@gmail.com>
2020-06-09 22:20:47 -0700

    refactored remove function to properly remove the relation

--- routers/avsRouter.js
+++ routers/avsRouter.js
@@ -3,43 +3,20 @@ const router = express.Router();
 
 const AVS = require('../models/attrVoiceSampleModel.js');
 
-// add association
-router.post('/', (req, res) => {
-  const data = req.body;
-  AVS.add(data)
-    .then(saved => {
-      res.status(201).json(saved);
+// remove association
+router.delete('/', (req, res) => {
+  console.log(req.body);
+  const { vsID, attrTitle } = req.body;
+  AVS.remove(vsID, attrTitle)
+    .then(removed => {
+      res.status(200).json(removed)
     })
     .catch(err => {
       res.status(500).json({
-        message: "Could not create association",
-        error: err.message
-      });
-    });
-})
-
-// remove association
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  AVS.findById(id)
-    .then(
-      AVS.remove(id)
-        .then(deleted => {
-          res.status(200).json(deleted);
-        })
-        .catch(err => {
-          res.status(500).json({
-            message: "Could not delete association",
-            error: err.message
-          });
-        })
-    )
-    .catch(err => {
-      res.status(400).json({
-        message: `Could not find assocation with ID: ${id}`,
-        error: err.message
-      });
-    });
+        message: 'Could not remove relation',
+        error: err
+      })
+    })
 })
 
 module.exports = router;
